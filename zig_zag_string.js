2aef905de4288feef616dc8a475da64b5bf98422
youngcitrus <mrjustinnguyen@gmail.com>
2020-02-20 16:55:17 -0800

    zig zag string algo

---
+++ zig_zag_string.js
@@ -0,0 +1,20 @@
+function convert(s, numRows) {
+    if (numRows === 1) {
+      return s;
+    }
+  
+    const N = s.length;
+    const arr = [...Array(numRows)].map(r => []);
+  
+    for (let i = 0; i < N; i++) {
+      const pos = i % (2*numRows-2);
+      const ii = pos < numRows ? pos : 2*numRows-2-pos;
+      arr[ii].push(s[i]);
+        if (i === 3) {
+            console.log(pos)
+            console.log(arr);
+        }
+    }
+    
+    return arr.map(r => r.join('')).join('');
+  }
