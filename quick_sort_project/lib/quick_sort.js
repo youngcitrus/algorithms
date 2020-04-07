27704c522abaf2f10f442df9d9de50f448439d66
youngcitrus <mrjustinnguyen@gmail.com>
2020-04-07 13:24:19 -0700

    quicksort algo

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,9 @@
 function quickSort(array) {
-
+    if (array.length <= 1) return array;
+    let pivot = array.pop();
+    let left = array.filter((el) => el < pivot);
+    let right = array.filter((el) => el >= pivot);
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 
