455572c1630da58fcbdce9cac237e659e78c7f6d
youngcitrus <mrjustinnguyen@gmail.com>
2020-04-07 00:30:00 -0700

    leetcode problems solved for tabulation project

--- tabulation_project/lib/leet_code_70.js
+++ tabulation_project/lib/leet_code_70.js
@@ -2,5 +2,15 @@
 // Feel free to use this file for scratch work.
 
 function climbStairs(n) {
-
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    let table = new Array(n);
+    table[0] = 1;
+    table[1] = 2;
+    for (let i = 2; i< n; i++) {
+        table[i] = table[i-1] + table[i-2];
+    }
+    return table[n-1];
+    
+    // this is just fibonacci sequence!
 }
