651661a465c437c529ed3ca84498a2f01d357ae7
youngcitrus <mrjustinnguyen@gmail.com>
2020-04-07 13:16:15 -0700

    merge sort algo

--- merge_sort_project/lib/merge_sort.js
+++ merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,18 @@
 function merge(array1, array2) {
-
+    const merged = [];
+    while (array1.length && array2.length) {
+        if (array1[0] < array2[0]) merged.push(array1.shift());
+        else merged.push(array2.shift());
+    }
+    return merged.concat(array1).concat(array2);
 }
 
 function mergeSort(array) {
-
+    if (array.length <= 1) return array;
+    let mid = Math.floor(array.length / 2);
+    let left = array.slice(0,mid);
+    let right = array.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 module.exports = {
