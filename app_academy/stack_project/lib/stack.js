61fb2ee863dbceeaa989acf295279deabc87fd91
youngcitrus <mrjustinnguyen@gmail.com>
2020-04-13 11:07:57 -0700

    build stack with linked list

--- app_academy/stack_project/lib/stack.js
+++ app_academy/stack_project/lib/stack.js
@@ -21,11 +21,49 @@
 // -----------
 
 class Node {
-
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class Stack {
+    constructor() {
+        this.length = 0;
+        this.top = null;
+        this.bottom = null;
+    }
 
+    push(value) {
+        let newNode = new Node(value)
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            newNode.next = this.top;
+            this.top = newNode;
+        }
+        this.length += 1;
+        return this.length;
+    }
+
+    pop() {
+        if (this.length === 0) return null;
+        let removedNode = this.top;
+        if (this.top === this.bottom) {
+            this.top = null;
+            this.bottom = null;
+        } else {
+            this.top = removedNode.next;
+        }
+        removedNode.next = null;
+        this.length -= 1;
+        return removedNode.value;
+    }
+
+    size() {
+        return this.length;
+    }
 }
 
 exports.Node = Node;
