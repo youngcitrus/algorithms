4a4f0debb201ab67e49f025c72a8ca43753ebe19
youngcitrus <mrjustinnguyen@gmail.com>
2020-04-14 17:01:57 -0700

    complete tree exercise

--- app_academy/tree_order_project/lib/leet_code_105.js
+++ app_academy/tree_order_project/lib/leet_code_105.js
@@ -5,5 +5,19 @@ const { TreeNode } = require('./tree_node.js');
 
 
 function buildTree(preorder, inorder) {
-
+    if (preorder.length === 0 && inorder.length === 0) return null;
+    
+    let root = new TreeNode(preorder[0]);
+    
+    let midIdx = inorder.indexOf(root.val);
+    let leftInOrder = inorder.slice(0, midIdx);
+    let rightInOrder = inorder.slice(midIdx + 1);
+    
+    let leftPreOrder = preorder.filter((val) => leftInOrder.includes(val));
+    let rightPreOrder = preorder.filter((val) => rightInOrder.includes(val));
+    
+    root.left = buildTree(leftPreOrder, leftInOrder);
+    root.right = buildTree(rightPreOrder, rightInOrder);
+    
+    return root;
 }
--- app_academy/tree_order_project/lib/tree_order.js
+++ app_academy/tree_order_project/lib/tree_order.js
@@ -1,9 +1,23 @@
 function inOrderArray(root) {
+    if (!root) return [];
+    // let array = [];
+    // array = array.concat(inOrderArray(root.left));
+    // array.push(root.val);
+    // array = array.concat(inOrderArray(root.right));
+    // return array;
+
+    return [...inOrderArray(root.left), root.val, ...inOrderArray(root.right)]
 
 }
 
 function postOrderArray(root) {
-
+    if (!root) return [];
+    // let array = [];
+    // array.push(...postOrderArray(root.left))
+    // array.push(...postOrderArray(root.right));
+    // array.push(root.val);
+    // return array;
+    return [...postOrderArray(root.left), ...postOrderArray(root.right), root.val]
 }
 
 
