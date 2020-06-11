c2fd1836210513a66c9f082ed21d82a2f7eabd2f
youngcitrus <mrjustinnguyen@gmail.com>
2020-06-10 17:30:55 -0700

    bst iterator leetcode

---
+++ leetcode/bstIterator.js
@@ -0,0 +1,36 @@
+var BSTIterator = function(root) {
+    this.values = [];
+    this.pointer = 0;
+    if (!root) return;
+    
+    this._inorder(root);
+    
+    
+    
+    // console.log(this.values)
+};
+
+BSTIterator.prototype._inorder = function(root) {
+    if (!root) return;
+    this._inorder(root.left);
+    this.values.push(root.val);
+    this._inorder(root.right);
+}
+
+/**
+ * @return the next smallest number
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+    const nextValue = this.values[this.pointer];
+    this.pointer += 1;
+    return nextValue;
+};
+
+/**
+ * @return whether we have a next smallest number
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+    return this.pointer < this.values.length;
+};
