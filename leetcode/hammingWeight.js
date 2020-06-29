aaf624404b5e7db5bfddef99d9e1253b0dfbf648
youngcitrus <mrjustinnguyen@gmail.com>
2020-06-29 11:02:04 -0700

    add bit manipulation optimizations

--- leetcode/hammingWeight.js
+++ leetcode/hammingWeight.js
@@ -11,12 +11,25 @@ var hammingWeight = function(n) {
 
 // Using bit manipulation
 
+// using bit mask
+
 var hammingWeight = function(n) {
     let count = 0;
     let mask = 1;
     for (let i=0; i<32; i++) {
         if ((n & mask) !== 0) count += 1;
         mask <<= 1;
+    }
+    return count;
+};
+
+// using bit flipping
+
+var hammingWeight = function(n) {
+    let count = 0;
+    while (n !== 0) {
+        count += 1;
+        n &= (n - 1)
     }
     return count;
 };
