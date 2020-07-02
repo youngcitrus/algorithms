e7b7f70910335ecdbd3c32b6f57e55d23ca7a9cc
youngcitrus <mrjustinnguyen@gmail.com>
2020-07-01 21:52:18 -0700

    syntax

--- leetcode/maxSumNoAdjacent.js
+++ leetcode/maxSumNoAdjacent.js
@@ -5,11 +5,11 @@ var rob = function(nums) {
     let bestSums = new Array(nums.length - 1);
     // bestSums[0] = nums[0];
     // bestSums[1] = Math.max(nums[0], nums[1]);
-    let prevPrevBest = nums[0]
+    let prevPrevBest = nums[0];
     let prevBest = Math.max(nums[0], nums[1]);
     let currentBest = prevBest;
     for (let i = 2; i < nums.length; i++) {
-        currentBest = Math.max(prevBest, prevPrevBest + nums[i])
+        currentBest = Math.max(prevBest, prevPrevBest + nums[i]);
         prevPrevBest = prevBest;
         prevBest = currentBest;
     }
