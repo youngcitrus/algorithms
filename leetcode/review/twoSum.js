7284d257339163eea5a02027820421648dd89793
youngcitrus <mrjustinnguyen@gmail.com>
2020-07-08 13:48:01 -0700

    review algorithms

---
+++ leetcode/review/twoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+    const seen = {};
+    for (let i=0; i<nums.length; i++) {
+        let diff = target - nums[i];
+        if (diff in seen) {
+            return [seen[diff], i];
+        }
+        seen[nums[i]] = i;
+    }
+    return -1;
+};
