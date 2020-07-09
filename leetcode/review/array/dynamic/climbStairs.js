3094bf7c1d4a2ff9efb68cc5cde6efaee2148084
youngcitrus <mrjustinnguyen@gmail.com>
2020-07-09 14:19:54 -0700

    coin change algo

---
+++ leetcode/review/array/dynamic/climbStairs.js
@@ -0,0 +1,5 @@
+var climbStairs = function(n, memo = {0: 1, 1: 1}) {
+    if (n in memo) return memo[n];
+    memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+    return memo[n];
+};
---
+++ leetcode/review/array/dynamic/coinChange.js
@@ -0,0 +1,10 @@
+var coinChange = function(coins, amount) {
+    const dp = new Array(amount+1).fill(Infinity);
+    dp[0] = 0;
+    for (coin of coins) {
+        for (let i = coin; i<amount+1; i++) {
+            dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+        }
+    }
+    return dp[amount] !== Infinity ? dp[amount] : -1;
+};
