a052b24db6fae25d42f93793313140ae2f149663
youngcitrus <mrjustinnguyen@gmail.com>
2020-07-09 12:40:27 -0700

    more algos

---
+++ leetcode/review/bestTimeToBuySellStock.js
@@ -0,0 +1,14 @@
+var maxProfit = function(prices) {
+    // if (!(prices.length)) return 0;
+    let lowest = prices[0];
+    let greatestDiff = 0;
+    for (let i=1; i<prices.length; i++) {
+        if (prices[i] < lowest) {
+            lowest = prices[i];
+        } else {
+            let diff = prices[i] - lowest;
+            greatestDiff = greatestDiff < diff ? diff : greatestDiff;
+        }
+    }
+    return greatestDiff;
+};
